754959904a3b5cf53d0be9f230b9078c4bce92eb
geoffrey woodcock <theatomproject@gmail.com>
2018-01-13 23:56:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = new Object({Key: "0"});
 
 function updatePlaylist(playlist, aristsName, songTitle){
-  playlist[radiohead] = creep
+  playlist[artistName] = songTitle
   return playlist
 }
 function removeFromPlaylist(playlist, aristName){
